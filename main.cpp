095a4c3ad2b26a6c872c7097b886766c713eef58
zhe <3144535872@qq.com>
2026-03-27 00:48:38 -0400

    Please grade homework9 windows

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 #include "Helper.h"
 #include <chrono>
 #include <iostream>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
+    std::cout<< "Starting game..." << std::endl;
+    // using chorono to measure runtime
+    auto start = std::chrono::high_resolution_clock::now();
     GameEngine game;
     game.gameLoop();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout<< "Exiting game..." << std::endl;
+    std::cout<<"Run time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
     return 0;
 }
